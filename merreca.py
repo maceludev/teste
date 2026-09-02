ad87b6b3450b2f8216879ee3ea24cfe03fdd4e8a
AM4ROG <canaltrivelistas@gmail.com>
2026-09-02 08:07:20 -0300

    fix code merreca

--- merreca.py
+++ merreca.py
@@ -1,24 +1,14 @@
 merrecas = float(input("Digite o valor de merrecas (M$): "))
-qnt_cem_merrecas, qnt_cinq_merrecas, qnt_dez_merrecas, qnt_cinco_merrecas, qnt_uma_merreca = 0, 0, 0, 0, 0
 
-if (merrecas >= 100):
-    qnt_cem_merrecas = int(merrecas // 100)
-    merrecas %= 100
-    if (merrecas != 0):
-        qnt_cinq_merrecas = int(merrecas // 50)
-        merrecas %= 50
-        if (merrecas != 0):
-            qnt_dez_merrecas = int(merrecas // 10)
-            merrecas %= 10
-            if (merrecas != 0):
-                qnt_cinco_merrecas = int(merrecas // 5)
-                merrecas %= 5
-                if (merrecas != 0):
-                    qnt_uma_merreca = int(merrecas // 1)
+qnt_cem = merrecas // 100
+qnt_cinq = merrecas % 100 // 50
+qnt_dez = merrecas % 100 % 50 // 10
+qnt_cinco = merrecas % 100 % 50 % 10 // 5
+qnt_um = merrecas % 100 % 50 % 10 % 5
 
-print(f"{qnt_cem_merrecas} moeda(s) de M$ 100,00")
-print(f"{qnt_cinq_merrecas} moeda(s) de M$ 50,00")
-print(f"{qnt_dez_merrecas} moeda(s) de M$ 10,00")
-print(f"{qnt_cinco_merrecas} moeda(s) de M$ 5,00")
-print(f"{qnt_uma_merreca} moeda(s) de M$ 1,00")
+print(f"{qnt_cem} moeda(s) de M$ 100,00")
+print(f"{qnt_cinq} moeda(s) de M$ 50,00")
+print(f"{qnt_dez} moeda(s) de M$ 10,00")
+print(f"{qnt_cinco} moeda(s) de M$ 5,00")
+print(f"{qnt_um} moeda(s) de M$ 1,00")
 
