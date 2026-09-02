1bb55faecde38532f98d7e20f3c146ba9e3e6373
AM4ROG <canaltrivelistas@gmail.com>
2026-09-02 08:26:19 -0300

    fix code merreca

--- merreca.py
+++ merreca.py
@@ -6,9 +6,11 @@ qnt_dez = merrecas % 100 % 50 // 10
 qnt_cinco = merrecas % 100 % 50 % 10 // 5
 qnt_um = merrecas % 100 % 50 % 10 % 5
 
-print(f"{qnt_cem} moeda(s) de M$ 100,00")
-print(f"{qnt_cinq} moeda(s) de M$ 50,00")
-print(f"{qnt_dez} moeda(s) de M$ 10,00")
-print(f"{qnt_cinco} moeda(s) de M$ 5,00")
-print(f"{qnt_um} moeda(s) de M$ 1,00")
+qnts = [qnt_cem, qnt_cinq, qnt_dez, qnt_cinco, qnt_um]
+
+moedas = [100, 50, 10, 5, 1]
+
+for i in range(len(qnts)):
+    print(f"{int(qnts[i])} moeda(s) de M$ {int(moedas[i])},00")
+
 
